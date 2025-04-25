576006f836da937b1794f86ba7a8d7b795574afd
Fernando R. G <nando.rodriguez.gomez89@gmail.com>
2025-04-25 22:10:26 +0000

    Ejercicio del generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let sujeto = [
+    "Un perro",
+    "Un dinosaurio",
+    "El hombre del saco",
+    "Un extraterrestre"
+  ];
+  let verbo = ["se comió", "se durmió en", "tropezó con", "se compró"];
+  let objeto = [
+    "una lata de atún",
+    "una hamburguesa vegana",
+    "mi tarjeta del metro",
+    "unas Air Max del 92"
+  ];
+  let accion = ["me secuestró", "se desintegró", "explotó", "me hipnotizó"];
+
+  let rnm1 = Math.floor(Math.random() * sujeto.length);
+  let rnm2 = Math.floor(Math.random() * verbo.length);
+  let rnm3 = Math.floor(Math.random() * objeto.length);
+  let rnm4 = Math.floor(Math.random() * accion.length);
+
+  document.querySelector("#excuse").innerHTML =
+    sujeto[rnm1] +
+    " " +
+    verbo[rnm2] +
+    " " +
+    objeto[rnm3] +
+    " y " +
+    accion[rnm4] +
+    ".";
 };
